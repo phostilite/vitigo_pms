d7aa6b1c8050a214a100e61bb29cf523a610ec87
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-10-16 21:24:09 +0530

    Comment out django_ratelimit in settings.py
    - Commented out `django_ratelimit` in `INSTALLED_APPS` in `settings.py` to disable rate limiting.

--- vitigo_pms/settings.py
+++ vitigo_pms/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     # Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
-    'django_ratelimit',
+    # 'django_ratelimit',
 
     # Custom apps
     'user_management',
